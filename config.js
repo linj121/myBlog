32e850d67ff92e3f41c37cac25da4c21b9aed758
jiexu <linj121@mcmaster.ca>
2023-06-19 19:54:35 -0400

    chore: First time commit the whole project

---
+++ config.js
@@ -0,0 +1,4 @@
+const PASSWORD = "18401949rxsgddT$";
+const MONGODB_URI = `mongodb+srv://linj121:${PASSWORD}@todolistdb.myjef98.mongodb.net/blogDB?retryWrites=true&w=majority`;
+
+exports.MONGODB_URI = MONGODB_URI;
